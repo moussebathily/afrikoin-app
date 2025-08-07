1307c6c470845adee35aa95b0919ab6babe6e025
Mousse bathily <bathilymousse24@gmail.com>
2025-08-07 18:43:28 +0200

    Update vite.config.ts
    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,17 +7,22 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/composants'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@types': path.resolve(__dirname, 'src/types'),
+    },
   },
-  define: {
-    global: {}
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/global.scss";`,
+      },
+    },
   },
   server: {
-    port: 3000
+    port: 3000,
+    open: true,
   },
-  build: {
-    outDir: 'dist',
-    sourcemap: true
-  }
 })
