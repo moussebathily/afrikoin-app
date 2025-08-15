65f571169329c249c944560cb7b0ede69f44f7f8
Mousse bathily <bathilymousse24@gmail.com>
2025-08-16 01:36:01 +0200

    fix: correct tailwind dark mode and plugin import

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,9 @@
 
 import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
-	darkMode: ["class"],
+        darkMode: "class",
 	content: [
 		"./pages/**/*.{ts,tsx}",
 		"./components/**/*.{ts,tsx}",
@@ -109,5 +110,5 @@ export default {
 			}
 		}
 	},
-	plugins: [require("tailwindcss-animate")],
+        plugins: [tailwindcssAnimate],
 } satisfies Config;
